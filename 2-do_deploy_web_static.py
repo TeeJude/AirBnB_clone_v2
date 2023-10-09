6ba41d6ec28409496388fc89929f8d09ac17c526
TeeJude <teejayjude@gmail.com>
2023-10-08 19:39:18 -0700

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = [""54.172.56.188, "100.25.118.208"]
+env.hosts = ["54.172.56.188", "100.25.118.208"]
 env.user = "ubuntu"
 
 def do_pack():
